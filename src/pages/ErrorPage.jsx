69aceed9e219b8107cc131ad230bd7d3ca3e0a91
Andrei G <garnaga.and@gmail.com>
2023-09-21 16:50:24 +0300

    Прописал стили ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,14 +2,40 @@ import React from "react";
 
 const ErrorPage = () => {
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-      <img
-        width={150}
-        height={150}
-        src="../../public/img/emoji-sorry.png"
-        alt="emoji-sorry"
-      />
-      <h4>Упс, что то пошло не по плану ;(</h4>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div className="emoji__wrapper">
+        <img
+          style={{
+            marginTop: "200px",
+            borderRadius: "50%",
+            boxShadow: "0 5px 20px #371A45",
+          }}
+          width={200}
+          height={200}
+          src="/img/interstellar-cry.gif"
+          alt="emoji-sorry"
+        />
+      </div>
+      <h4
+        style={{
+          fontSize: 20,
+          fontWeight: 600,
+          color: "#371A45",
+          width: "200px",
+          marginTop: 30,
+          textAlign: "center",
+        }}
+      >
+        Упс! Что то пошло не по плану ;(
+      </h4>
     </div>
   );
 };
